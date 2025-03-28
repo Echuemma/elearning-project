3280be4c2ee8db748a272ed3c405c8a9aeefa7df
Echuemmanuel <echuemmanuel918@gmail.com>
2025-03-28 06:57:01 +0100

    made chnages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import { BrowserRouter } from "react-router-dom";
 import BaseRouter from "./routes";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    AOS.init({ duration: 1000, easing: "ease-in-out", once: true });
+  }, []);
+
   return (
-    
-        <BrowserRouter>
-          <BaseRouter />
-        </BrowserRouter>
-      
+    <BrowserRouter>
+      <BaseRouter />
+    </BrowserRouter>
   );
 }
 
